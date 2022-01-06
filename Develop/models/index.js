8606e2e4035b04e65d590c75f55c1539dd3a4315
Brian Smith <bjtsmith23@gmail.com>
2022-01-05 21:30:21 -0500

    index.js model updated

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,29 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE"
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongstoMany(Tag, {
+  foreignKey: "product_id",
+  through: ProductTag
+
+})
 
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongstoMany(Product, {
+  foreignKey: "tag_id",
+  through: ProductTag
+})
 
 module.exports = {
   Product,
